d239ce1692131896f8e459260166de8b27daa834
peaksira <sirawitnontakanok@gmail.com>
2026-02-27 12:41:52 +0700

    add frontend

--- backend/main.py
+++ backend/main.py
@@ -64,7 +64,6 @@ class Comment(db.Model):
     #db.create_all()
 
 
-
 @app.route('/api/todos/', methods=['GET'])
 @jwt_required()
 def get_todos():
@@ -148,8 +147,3 @@ def login():
 
     access_token = create_access_token(identity=user.username)
     return jsonify(access_token=access_token)
-@app.route('/api/todos/', methods=['GET'])
-@jwt_required()
-def get_todos():
-    todos = TodoItem.query.all()
-    return jsonify([todo.to_dict() for todo in todos])
